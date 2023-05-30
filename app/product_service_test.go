f222c32c7087cc0fa3017e38a46bf2b85d0fb319
salesof7 <jpsaless2002@gmail.com>
2023-05-30 14:02:24 -0300

    feat: implement Create in product service

--- app/product_service_test.go
+++ app/product_service_test.go
@@ -16,12 +16,27 @@ func TestProductService_Get(t *testing.T) {
 	product := mock_app.NewMockProductInterface(ctrl)
 	persistence := mock_app.NewMockProductPersistenceInterface(ctrl)
 	persistence.EXPECT().Get(gomock.Any()).Return(product, nil).AnyTimes()
-
 	service := app.ProductService{
 		Persistence: persistence,
 	}
 
 	result, err := service.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+}
+
+func TestProductService_Create(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_app.NewMockProductInterface(ctrl)
+	persistence := mock_app.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+	service := app.ProductService{
+		Persistence: persistence,
+	}
+
+	result, err := service.Create("Product 1", 10)
 	require.Nil(t, err)
 	require.Equal(t, product, result)
 }
