6e31c7d49ac97030fb389105f445937ad0ab9b31
salesof7 <jpsaless2002@gmail.com>
2023-05-30 14:13:24 -0300

    feat: implement Enable and Disable in product service

--- app/product_service_test.go
+++ app/product_service_test.go
@@ -40,3 +40,27 @@ func TestProductService_Create(t *testing.T) {
 	require.Nil(t, err)
 	require.Equal(t, product, result)
 }
+
+func TestProductService_EnableDisable(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_app.NewMockProductInterface(ctrl)
+	product.EXPECT().Enable().Return(nil)
+	product.EXPECT().Disable().Return(nil)
+
+	persistence := mock_app.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := app.ProductService{
+		Persistence: persistence,
+	}
+
+	result, err := service.Enable(product)
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+
+	result, err = service.Disable(product)
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+}
