66e6d779f1a971b4f0d4c50dcfcacb6c95bcd586
salesof7 <jpsaless2002@gmail.com>
2023-05-30 01:14:25 -0300

    feat: implement methods for the Product struct based on the ProductInterface

---
+++ app/product_test.go
@@ -0,0 +1,60 @@
+package app_test
+
+import (
+	"testing"
+
+	"github.com/salesof7/studies_go-hexagonal/app"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestProduct_Enable(t *testing.T) {
+	product := app.Product{}
+	product.Name = "Hello"
+	product.Status = app.DISABLED
+	product.Price = 10
+
+	err := product.Enable()
+	require.Nil(t, err)
+
+	product.Price = 0
+	err = product.Enable()
+	require.Equal(t, "the price must be greater than zero to enable the product", err.Error())
+}
+
+func TestProduct_Disable(t *testing.T) {
+	product := app.Product{}
+	product.Name = "Hello"
+	product.Status = app.ENABLED
+	product.Price = 0
+
+	err := product.Disable()
+	require.Nil(t, err)
+
+	product.Price = 10
+	err = product.Disable()
+	require.Equal(t, "the price must be zero in order to have the product disabled", err.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := app.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "Hello"
+	product.Status = app.DISABLED
+	product.Price = 10
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "INVALID"
+	_, err = product.IsValid()
+	require.Equal(t, "the status must be enabled or disabled", err.Error())
+
+	product.Status = app.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "the price must be greater or equal zero", err.Error())
+}
